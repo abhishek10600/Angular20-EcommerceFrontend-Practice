bfb40fd8899c8569384086be4892444be7c1fe1a
abhishek10600 <abhi10sheksharma@gmail.com>
2025-11-02 22:42:38 +0530

    First Commit. Header Component created and routing has been setup. Project initialized, tailwindcss and angular material initialized.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import ProductsGrid from './pages/products-grid/products-grid';
+import MyWishlist from './pages/my-wishlist/my-wishlist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "products"
+    },
+    {
+        path: "products",
+        loadComponent: ()=> ProductsGrid
+    },
+    {
+        path: "wishlist",
+        loadComponent: ()=> MyWishlist
+    }
+];
