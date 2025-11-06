854dc3adb857538975bc460ea7142da77707c4de
abhishek10600 <abhi10sheksharma@gmail.com>
2025-11-06 22:34:38 +0530

    Sidenav created. Used pipes as well

--- src/app/pages/products-grid/products-grid.ts
+++ src/app/pages/products-grid/products-grid.ts
@@ -1,25 +1,47 @@
 import { Component, computed, input, signal } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductCard } from '../../components/product-card/product-card';
+import { MatSidenavContainer, MatSidenavContent, MatSidenav } from "@angular/material/sidenav";
+import { MatNavList, MatListItem, MatListItemTitle } from "@angular/material/list";
+import { RouterLink } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ProductCard],
+  imports: [ProductCard, MatSidenavContainer, MatSidenavContent, MatSidenav, MatNavList, MatListItem, MatListItemTitle, RouterLink, TitleCasePipe],
   template: `
-    <div class="bg-gray-100 p-6 h-full">
-      <h1 class="text-2xl font-bold text-gray-900 mb-6">{{ category() }}</h1>
 
+  <mat-sidenav-container>
+    <mat-sidenav mode="side" opened="true">
+      <div class="p-6">
+        <h2 class="text-lg text-gray-900">Categories</h2>
+        <mat-nav-list>
+          @for (cat of categories(); track cat){
+            <mat-list-item [activated]="cat === category()" class="my-2" [routerLink]="['/products', cat]">
+                <span matListItemTitle class="font-medium" [class]="cat === category() ? '!text-white': null">{{ cat | titlecase}}</span>
+            </mat-list-item>
+          }
+        </mat-nav-list>
+      </div>
+    </mat-sidenav>
+    <mat-sidenav-content class="bg-gray-100 p-6 h-full">
+      <h1 class="text-2xl font-bold text-gray-900 mb-1">{{ category() | titlecase}}</h1>
+      <p class="text-base text-gray-600 mb-6">{{ filteredProducts().length }} products found</p>
       <div class="responsive-grid">
         @for (product of filteredProducts(); track product.id){
           <!-- <app-product-card [product]="product" (addToCartClicked)="addToCart($event)"/> -->
           <app-product-card [product]="product"/>
         }
       </div>
-    </div>
+    </mat-sidenav-content>
+  </mat-sidenav-container>
+    
   `,
   styles: ``,
 })
 export default class ProductsGrid {
+  
+  // this input is used to get the category from the url
   category = input<string>('all');
 
   products = signal<Product[]>([
@@ -91,6 +113,8 @@ export default class ProductsGrid {
     }
   }
   );
+
+  categories = signal<string[]>(["all", "electronics", "outdoors", "furniture", "home & office"])
 
   // addToCart(){
 
